39d1cc6b1ae6863b0d9a1a56abf43688dd3ffc80
MATSUMOTO Eiji <e.mattsan@gmail.com>
2025-07-26 11:07:19 +0900

    refactoring examples

--- examples/japanese_holidays.exs
+++ examples/japanese_holidays.exs
@@ -10,48 +10,86 @@ Mix.install([
   {:nan_no_hi, path: "."}
 ])
 
-{:ok, uri} = URI.new("https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv")
-filename = Path.basename(uri.path)
+defmodule JapaneseHolidays do
+  use GenServer
 
-# Make a new table
-table = NanNoHi.new()
+  def start_link(_ \\ []) do
+    csv = fetch_csv()
 
-# Download CSV file if it's not been downloaded
-if !File.exists?(filename) do
-  csv =
-    uri
-    |> Req.get!(raw: true)
-    |> then(&:iconv.convert("cp932", "utf-8", &1.body))
+    GenServer.start_link(__MODULE__, csv, name: __MODULE__)
+  end
 
-  File.write(filename, csv)
+  def lookup(year), do: GenServer.call(__MODULE__, {:lookup, {year}})
+  def lookup(year, month), do: GenServer.call(__MODULE__, {:lookup, {year, month}})
+  def lookup(year, month, day), do: GenServer.call(__MODULE__, {:lookup, {year, month, day}})
+
+  def init(csv) do
+    # Make a new table
+    table = NanNoHi.new(name: __MODULE__)
+
+    NanNoHi.import(table, csv)
+
+    {:ok, %{table: table}}
+  end
+
+  def handle_call({:lookup, date}, _from, state) do
+    result =
+      case date do
+        {year} -> NanNoHi.lookup(state.table, year)
+        {year, month} -> NanNoHi.lookup(state.table, year, month)
+        {year, month, day} -> NanNoHi.lookup(state.table, year, month, day)
+      end
+
+    {:reply, result, state}
+  end
+
+  defp fetch_csv do
+    {:ok, uri} = URI.new("https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv")
+    filename = Path.basename(uri.path)
+
+    # Download CSV file if it's not been downloaded
+    if File.exists?(filename) do
+      File.read!(filename)
+    else
+      csv = uri |> Req.get!(raw: true) |> then(&:iconv.convert("cp932", "utf-8", &1.body))
+
+      File.write!(filename, csv)
+
+      csv
+    end
+  end
 end
 
-# Read CSV
-{:ok, csv} = File.read(filename)
+defmodule ArgParser do
+  def parse(argv) do
+    case Enum.map(argv, &Integer.parse/1) do
+      [{year, ""}, {month, ""}, {day, ""}] ->
+        {:ok, JapaneseHolidays.lookup(year, month, day)}
 
-NanNoHi.import(table, csv)
+      [{year, ""}, {month, ""}] ->
+        {:ok, JapaneseHolidays.lookup(year, month)}
 
-options = System.argv()
+      [{year, ""}] ->
+        {:ok, JapaneseHolidays.lookup(year)}
 
-case Enum.map(options, &Integer.parse/1) do
-  [{year, ""}, {month, ""}, {day, ""}] ->
-    {:ok, NanNoHi.lookup(table, year, month, day)}
+      _ ->
+        {:error, error_message(argv)}
+    end
+  end
 
-  [{year, ""}, {month, ""}] ->
-    {:ok, NanNoHi.lookup(table, year, month)}
-
-  [{year, ""}] ->
-    {:ok, NanNoHi.lookup(table, year)}
-
-  _ ->
-    usage = """
-    Invalid options #{inspect(options)}
+  defp error_message(argv) do
+    """
+    Invalid options #{inspect(argv)}
 
     usage: elixir examples/japanese_holidays.exs <year> [<month> [<day>]]
     """
-
-    {:error, usage}
+  end
 end
+
+{:ok, _} = JapaneseHolidays.start_link()
+
+System.argv()
+|> ArgParser.parse()
 |> case do
   {:ok, []} ->
     IO.puts("No events")
