6a3ebfd9994d779d243f528261eda2cf0bff6695
MATSUMOTO Eiji <e.mattsan@gmail.com>
2025-07-26 18:29:30 +0900

    fix append functions

--- lib/nan_no_hi.ex
+++ lib/nan_no_hi.ex
@@ -31,6 +31,16 @@ defmodule NanNoHi do
   `append(table, ~D[2025-07-15], "Tuesday")` is equivalent to `append(table, 2025, 7, 15, "Tuesday")`.
 
   See `append/5` for the version that takes year, month, day, and description separately.
+
+  ## Examples
+
+  ```elixir
+  iex> table = NanNoHi.new()
+  iex> NanNoHi.append(table, ~D[2025-01-01], "元日")
+  iex> NanNoHi.append(table, ~D[2025-05-05], "こどもの日")
+  iex> NanNoHi.lookup(table, 2025)
+  [{~D[2025-01-01], "元日"}, {~D[2025-05-05], "こどもの日"}]
+  ```
   """
   @spec append(:ets.table(), Date.t(), term()) :: :ok
   def append(table, date, description) when is_struct(date, Date) do
@@ -41,12 +51,37 @@ defmodule NanNoHi do
 
   @doc """
   Appends a new event.
-  """
-  @spec append(:ets.table(), year(), month(), day(), term()) :: :ok
-  def append(table, year, month, day, description) when is_date(year, month, day) do
-    :ets.insert(table, {{year, month, day}, description})
 
-    :ok
+  ## Examples
+
+  ```elixir
+  iex> table = NanNoHi.new()
+  iex> NanNoHi.append(table, 2025, 1, 1, "元日")
+  :ok
+  iex> NanNoHi.append(table, 2025, 5, 5, "こどもの日")
+  :ok
+  iex> NanNoHi.lookup(table, 2025)
+  [{~D[2025-01-01], "元日"}, {~D[2025-05-05], "こどもの日"}]
+  ```
+
+  ```elixir
+  iex> table = NanNoHi.new()
+  iex> NanNoHi.append(table, 2025, 13, 31, "INVALID")
+  {:error, :invalid_date}
+  iex> NanNoHi.append(table, 2025, 12, 32, "INVALID")
+  {:error, :invalid_date}
+  iex> NanNoHi.append(table, 2025, 2, 29, "INVALID")
+  {:error, :invalid_date}
+  ```
+  """
+  @spec append(:ets.table(), year(), month(), day(), term()) :: :ok | {:error, :invalid_date}
+  def append(table, year, month, day, description) do
+    if is_date(year, month, day) && :calendar.valid_date(year, month, day) do
+      :ets.insert(table, {{year, month, day}, description})
+      :ok
+    else
+      {:error, :invalid_date}
+    end
   end
 
   @doc """
@@ -198,6 +233,20 @@ defmodule NanNoHi do
 
   @doc """
   Clears all events.
+
+  ## Examples
+
+  ```elixir
+  iex> table = NanNoHi.new()
+  iex> NanNoHi.append(table, 2025, 1, 1, "元日")
+  iex> NanNoHi.append(table, 2025, 5, 5, "こどもの日")
+  iex> NanNoHi.lookup_all(table)
+  [{~D[2025-01-01], "元日"}, {~D[2025-05-05], "こどもの日"}]
+  iex> NanNoHi.clear(table)
+  :ok
+  iex> NanNoHi.lookup_all(table)
+  []
+  ```
   """
   @spec clear(:ets.table()) :: :ok
   def clear(table) do
